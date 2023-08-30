3393ae9a656d06b8417a32c1ff9ffcebdffed135
Laisa <laisacristina2606@gmail.com>
2023-08-30 10:19:37 -0300

    feat: folder structure update

---
+++ adapter/http/transaction/main.go
@@ -0,0 +1,48 @@
+package transaction
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/LaisaCCAndrade/expert-session-finance/model/transaction"
+)
+
+func GetTransactions(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+
+	var layout = "2006-01-02T15:04:05"
+	salaryReceived, _ := time.Parse(layout, "2023-08-30T06:55:25")
+
+	var transactions = transaction.Transactions{
+		transaction.Transaction{
+			Title:     "Salário",
+			Amount:    1200.0,
+			Type:      0,
+			CreatedAt: salaryReceived,
+		},
+	}
+
+	_ = json.NewEncoder(w).Encode(transactions)
+}
+
+func CreateATransaction(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var res = transaction.Transactions{}
+	var body, _ = io.ReadAll(r.Body)
+
+	_ = json.Unmarshal(body, &res)
+
+	fmt.Println(res)
+}
--- main.go
+++ main.go
@@ -1,43 +1,7 @@
 package main
 
-import (
-	"net/http"
-	"time"
-)
+import "github.com/LaisaCCAndrade/expert-session-finance/adapter/http"
 
 func main() {
-
-	http.HandleFunc("/", getTransactions)
-
-	http.ListenAndServe(":8080", nil)
-
-}
-
-type Transaction struct {
-	Title string
-	Amount float32
-	Type int
-	CreatedAt time.Time
-}
-
-type Transactions []Transaction
-
-func getTransactions(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	var layout = "2006-01-02"
-	time.Parse()
-
-	var transactions = Transactions{
-		Transaction{
-			Title: "Salário",
-			Amount: 1200.0,
-			Type: 0,
-			CreatedAt: ,
-		}
-	}
-	
+	http.Init()
 }
